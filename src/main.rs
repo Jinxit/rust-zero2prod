4decdfde39638704e8c670b3e3eb9b79d5a68edb
Lucas Åström <lucasas@kth.se>
2021-12-17 16:11:29 +0100

    health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/health_check")]
+async fn health_check() -> () {
+    ()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![
+            health_check
+        ])
 }
