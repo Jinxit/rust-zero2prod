581720a6546d7a0844f400203fde25e034dba9a4
Lucas Åström <lucasas@kth.se>
2021-12-27 12:56:30 +0100

    instrument tracing using macros, and refactor subscribe route

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,7 +4,6 @@ use chrono::Utc;
 use diesel::RunQueryDsl;
 use rocket::form::Form;
 use rocket::http::Status;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(FromForm)]
@@ -13,38 +12,41 @@ pub struct FormData {
     email: String,
 }
 
-#[post("/subscriptions", data = "<form>")]
-pub async fn subscribe(form: Form<FormData>, conn: NewsletterDbConn) -> Result<(), Status> {
-    use crate::schema::subscriptions;
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, conn),
+    fields(
+        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
-    );
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    let result: diesel::QueryResult<usize> = conn
-        .run(move |c| {
-            diesel::insert_into(subscriptions::table)
-                .values(NewSubscription {
-                    id: &Uuid::new_v4(),
-                    email: &form.email,
-                    name: &form.name,
-                    subscribed_at: &Utc::now(),
-                })
-                .execute(c)
-        })
-        .instrument(query_span)
-        .await;
-
-    match result {
+    )
+)]
+#[post("/subscriptions", data = "<form>")]
+pub async fn subscribe(form: Form<FormData>, conn: NewsletterDbConn) -> Result<(), Status> {
+    match insert_subscriber(form.into_inner(), conn).await {
         Ok(_) => Ok(()),
         Err(e) => {
             tracing::error!("Failed to execute query: {:?}", e);
             Err(Status::InternalServerError)
         }
     }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, conn)
+)]
+async fn insert_subscriber(form: FormData, conn: NewsletterDbConn) -> diesel::QueryResult<usize> {
+    use crate::schema::subscriptions;
+    conn.run(move |c| {
+        diesel::insert_into(subscriptions::table)
+            .values(NewSubscription {
+                id: &Uuid::new_v4(),
+                email: &form.email,
+                name: &form.name,
+                subscribed_at: &Utc::now(),
+            })
+            .execute(c)
+    })
+    .await
 }
